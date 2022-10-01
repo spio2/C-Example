7c68dd931636a911c970a09f860372a19acf66c7
LiAnna <spio@mail.ru>
2022-10-01 15:14:10 +0300

    add Example 005 IfElse

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "анна")
+{
+    Console.WriteLine("Ура, это же АННА!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
